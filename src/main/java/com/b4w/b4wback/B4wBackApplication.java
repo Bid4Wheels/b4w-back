176815eaed6cfdf88fd9105e8b80ec8eff82c015
manumasjoan <manumasjoan@gmail.com>
2023-08-11 14:14:36 -0300

    Initial commit

---
+++ src/main/java/com/b4w/b4wback/B4wBackApplication.java
@@ -0,0 +1,13 @@
+package com.b4w.b4wback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B4wBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B4wBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b4w/b4wback/B4wBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b4w.b4wback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B4wBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
