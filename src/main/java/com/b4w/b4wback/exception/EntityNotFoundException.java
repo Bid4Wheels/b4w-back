d0f1664dd5eeb6d467e68297f938467685f5ffdc
AlanFierr0 <85326611+AlanFierr0@users.noreply.github.com>
2023-08-21 13:20:50 -0300

    fix : Error handling

--- src/main/java/com/b4w/b4wback/exception/EntityNotFoundException.java
+++ src/main/java/com/b4w/b4wback/exception/EntityNotFoundException.java
@@ -1,6 +1,5 @@
 package com.b4w.b4wback.exception;
 
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String message) {
