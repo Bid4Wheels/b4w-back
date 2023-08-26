10ab1af8f325282fdb335e1bd668a7e996028f5c
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2023-08-26 01:30:06 -0300

    feat: created own validation for deadline restriction, and add some modifications for CreateAuctionDTO.

--- src/main/java/com/b4w/b4wback/dto/CreateAuctionDTO.java
+++ src/main/java/com/b4w/b4wback/dto/CreateAuctionDTO.java
@@ -3,7 +3,10 @@ package com.b4w.b4wback.dto;
 import com.b4w.b4wback.enums.AuctionStatus;
 import com.b4w.b4wback.enums.GasType;
 import com.b4w.b4wback.enums.GearShiftType;
+import com.b4w.b4wback.validation.DurationMinAfterCreation;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.validator.constraints.time.DurationMin;
@@ -13,45 +16,42 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@ToString
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class CreateAuctionDTO {
+    @NotNull(message = "User id can't be blank.")
+    private Long id;
     @NotBlank(message = "Description can't be blank.")
     @Size(max = 500, message = "The content must not exceed 500 characters.")
     private String description;
-
-    @NotBlank(message = "Deadline can't be blank.")
-    @DurationMin(hours = 24L,message = "Duration must be more o equals to 24 hours.")
+    @NotNull(message = "Deadline can't be null.")
+    @DurationMinAfterCreation
     private LocalDateTime deadline;
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt= LocalDateTime.now();
 
     @NotBlank(message = "Brand can't be blank.")
     private String brand;
     @NotBlank(message = "Model can't be blank.")
     private String model;
 
-    @NotBlank(message = "Base price can't be blank.")
+    @NotNull(message = "Base price can't be blank.")
     private Integer basePrice;
 
     private AuctionStatus status= AuctionStatus.OPEN;
 
-    @NotBlank(message = "Milage can't be blank.")
+    @NotNull(message = "Milage can't be blank.")
     private Integer milage;
 
-    @NotBlank(message = "Gas type can't be blank.")
+    @NotNull(message = "Gas type can't be blank.")
     private GasType gasType;
 
-    @NotBlank(message = "Year model can't be blank.")
+    @NotNull(message = "Year model can't be blank.")
     private Integer modelYear;
 
     @NotBlank(message = "Color can't be blank.")
     private String color;
 
-    @NotBlank(message = "Doors amount can't be blank.")
+    @NotNull(message = "Doors amount can't be blank.")
     private Integer doorsAmount;
 
-    @NotBlank(message = "Gear type can't be blank.")
+    @NotNull(message = "Gear type can't be blank.")
     private GearShiftType gearShiftType;
 }
