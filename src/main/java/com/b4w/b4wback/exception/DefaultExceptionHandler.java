6e1216bf4e0b7fdd66825da50e626b8ba90eec53
Franco B <franco.barravecchia2002@gmail.com>
2023-08-26 12:23:27 -0300

    feat: added exception handler for data integrity

--- src/main/java/com/b4w/b4wback/exception/DefaultExceptionHandler.java
+++ src/main/java/com/b4w/b4wback/exception/DefaultExceptionHandler.java
@@ -2,10 +2,10 @@ package com.b4w.b4wback.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -28,6 +28,13 @@ public class DefaultExceptionHandler {
                             return f.getField() + ": " + msg;
                         })
                 .reduce("", (a, s) -> a + s + '\n');
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    protected String handleDataIntegrityViolation(DataIntegrityViolationException ex){
+        return ex.getMessage();
     }
     @ExceptionHandler(EntityNotFoundException.class)
     protected ResponseEntity<Object> handleEntityNotFound(EntityNotFoundException exception) {
