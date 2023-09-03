659b1019368361d8ec70fc2d6013e556a75c2b5b
AlanFierr0 <85326611+AlanFierr0@users.noreply.github.com>
2023-09-03 13:59:27 -0300

    fix: Unuserd Imports

--- src/main/java/com/b4w/b4wback/service/interfaces/BidService.java
+++ src/main/java/com/b4w/b4wback/service/interfaces/BidService.java
@@ -1,7 +1,6 @@
 package com.b4w.b4wback.service.interfaces;
 
 import com.b4w.b4wback.dto.CreateBidDTO;
-import com.b4w.b4wback.model.Auction;
 import com.b4w.b4wback.model.Bid;
 
 public interface BidService {
