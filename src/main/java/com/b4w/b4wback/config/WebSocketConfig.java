82422b679e560efcd42ece1dfc5be1d4cc42550b
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2023-10-07 10:09:25 -0300

    feat: add websockets for Bid.

---
+++ src/main/java/com/b4w/b4wback/config/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.b4w.b4wback.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+@EnableWebSocketMessageBroker
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(org.springframework.web.socket.config.annotation.StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(org.springframework.messaging.simp.config.MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/auction");
+    }
+}
