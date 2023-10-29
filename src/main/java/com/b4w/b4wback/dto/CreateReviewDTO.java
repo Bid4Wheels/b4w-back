6075e45a8a95e34a4324b6305fc60bec1e094390
Franco B <franco.barravecchia2002@gmail.com>
2023-10-29 11:04:29 -0300

    fix: use ValidRatingValues

--- src/main/java/com/b4w/b4wback/dto/CreateReviewDTO.java
+++ src/main/java/com/b4w/b4wback/dto/CreateReviewDTO.java
@@ -1,5 +1,6 @@
 package com.b4w.b4wback.dto;
 
+import com.b4w.b4wback.validation.ValidRatingValues;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Size;
@@ -12,7 +13,6 @@ public class CreateReviewDTO {
     @Size(max = 400, min = 10)
     String review;
 
-    @DecimalMin(value = "1", message = "La puntuación mínima es 1")
-    @DecimalMax(value = "5", message = "La puntuación máxima es 5")
+   @ValidRatingValues
     float rating;
 }
