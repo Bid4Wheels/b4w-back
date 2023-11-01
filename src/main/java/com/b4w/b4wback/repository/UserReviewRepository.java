1500ecad8c30d2702499fd7aefc6bf05eafabf3e
Franco B <franco.barravecchia2002@gmail.com>
2023-11-01 15:30:16 -0300

    feat: finduserReviewFilter

--- src/main/java/com/b4w/b4wback/repository/UserReviewRepository.java
+++ src/main/java/com/b4w/b4wback/repository/UserReviewRepository.java
@@ -2,6 +2,11 @@ package com.b4w.b4wback.repository;
 
 import com.b4w.b4wback.model.UserReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserReviewRepository extends JpaRepository<UserReview, Long> {
+    @Query("SELECT ur FROM UserReview ur WHERE ur.punctuation > :punctuation AND (ur.reviewer.id = :userId OR ur.reviewed.id = :userId)")
+    List<UserReview> findUserReviewByPunctuationAfterAndReviewerIdOrReviewedId(float punctuation, long userId);
 }
